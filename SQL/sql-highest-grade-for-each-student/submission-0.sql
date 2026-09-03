92f6113f4f72c7d8805a1a504a169f4d84d842f6
Cameron Ball <67163069+camball@users.noreply.github.com>
2026-09-03 00:39:34 -0700

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,9 @@
+SELECT student_id, MIN(exam_id) AS exam_id, score
+FROM exam_results
+WHERE (student_id, score) IN (
+    SELECT student_id, MAX(score) AS score
+    FROM exam_results
+    GROUP BY student_id
+)
+GROUP BY student_id, score
+ORDER BY student_id ASC;
